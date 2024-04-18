de9dcf75d5fb8079f08f303a13efa9a8285cae0a
rosaTome <tomerosalinda@gmail.com>
2024-04-18 02:37:43 +0100

    EmployeeCardProps, EmployeeCard function and ex def added to EmployeeCard.tsx

---
+++ src/Components/EmployeeCard/EmployeeCard.tsx
@@ -0,0 +1,19 @@
+type EmployeeCardProps = {
+  id: number;
+  name: string;
+  role: string;
+}
+
+const EmployeeCard = ({id, name, role}: EmployeeCardProps)=> {
+  return (
+    <div className="employee-card">
+      <div className="employee-card__content">
+        <p className="employee-card__id">{id}</p>
+        <p className="employee-card__name">{name}</p>
+        <p className="employee-card__role">{role}</p>
+      </div>
+    </div>
+  )
+}
+
+export default EmployeeCard;
