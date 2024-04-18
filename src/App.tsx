40f75e663db10ac8e1490f405d090fbd4c47d788
rosaTome <tomerosalinda@gmail.com>
2024-04-18 02:55:55 +0100

    EmployeeCard added to App.tsx to test style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 // import React from 'react'
 
+import EmployeeCard from "./Components/EmployeeCard/EmployeeCard"
+import employees from "./data/employees"
+
 const App = () => {
   return (
     <div>
         <h1>Ticket Tracker</h1>
+        {employees && ( 
+            <EmployeeCard
+                id={`${employees[0].id}`}
+                name={`${employees[0].name}`}
+                role={`${employees[0].role}`}
+            />
+        )}
       
     </div>
   )
--- src/Components/EmployeeCard/EmployeeCard.tsx
+++ src/Components/EmployeeCard/EmployeeCard.tsx
@@ -1,4 +1,4 @@
-
+import "./EmployeeCard.scss";
 
 type EmployeeCardProps = {
   id: number;
